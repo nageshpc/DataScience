f5598b746c881b7b4c6bb63753e819bd4f05f767
nageshlaptop <nageshpc@gmail.com>
2018-06-24 11:28:15 +1000

    OSR 	included random retriever

--- question_answering/evaluate_answer_selector.py
+++ question_answering/evaluate_answer_selector.py
@@ -31,6 +31,17 @@ import pdb
 THRESHOLD = 0.5 
 ##
 
+
+##############################
+def find_ranks(candidates, scores):
+    scored_candidates= [ (candidates[idx], scores[idx]) for idx in range(len(scores)) ]
+    ranked_candidates= { 
+                        cid:  position + 1
+                        for position, (cid, score) in enumerate( sorted(scored_candidates , key= operator.itemgetter(1), reverse=True)  )
+                       }
+    return ranked_candidates
+###########
+
 def evaluate(model,  question_ids, candidate_ids,  test_labels, test_questions, test_answers) :
     print("Evaluating QA pair level scores")
     predicted_scores = model.predict( [test_questions, test_answers] )
@@ -59,46 +70,52 @@ def evaluate(model,  question_ids, candidate_ids,  test_labels, test_questions, 
         candidates_per_question[question_id].append(candidate_id)
     ###
     mean_prec, mean_rr = 0.0, 0.0 #mean average precision and mean reciprocal rank
+    mean_prec_random_model, mean_rr_random_model = 0.0, 0.0 # mean avg. precision and mean reciprocal rank of the random model
     valid_questions = 0.0 # a valid question is one with atleast one correct answer
 
     ##
-    pdb.set_trace()
     for question_id in set( question_ids ):
         true_y           = true_y_per_question[question_id]
         candidate_ids    = candidates_per_question[question_id]
         predicted_scores = pred_y_per_question[question_id]
+        ## FRM denotes from random model 
+        predicted_scores_random_model = [  np.random.uniform(0,1) for _ in predicted_scores ] 
         predicted_labels = [ 1 if score > THRESHOLD else 0 for score in predicted_scores]
        
         ## find the correct candidates from true labels
         correct_candidates = [candidate_ids[idx] for idx,true_label in enumerate(true_y) if true_label ==1.0]  
 
         #skip all invalid questions for evaluation
-        if len(correct_candidates) <= 0 : # if there is no correct answer :
+        if len(correct_candidates) <= 0  or len(correct_candidates)>= len(candidate_ids): # if NONE or ALL answers are correct, skip it
             continue 
         else:
             valid_questions += 1.0
         ####
- 
+
         ##
         average_precision = sklearn.metrics.average_precision_score (true_y, predicted_scores)
         mean_prec  += average_precision
+        mean_prec_random_model +=  sklearn.metrics.average_precision_score(true_y, predicted_scores_random_model)
         ##
-        ## find the rank of correct candidates in the predicted scores
-        # find candidate_ranking as per predicted scores  - order of scores in p corresponds to candidate id
-        candidate_scores_sorted = np.argsort(predicted_scores)
-        #map the candidates in ascending-order-positions to rank: position p -> rank (n-p) for p in (0,n-1)
-        candidate_id_to_rank= { candidate_ids[idx] : len(candidate_scores_sorted) - candidate_scores_sorted[idx] 
-                                for idx in range(len(candidate_scores_sorted)) } 
+        #find candidate rankings
+        candidate_id_to_rank     = find_ranks( candidate_ids , predicted_scores )
+        candidate_id_to_rank_random_model = find_ranks( candidate_ids , predicted_scores_random_model )
 
         #ranks of correctly retrieved answers
         ranks  = [ candidate_id_to_rank[candidate_id]  for candidate_id in correct_candidates]
-        reciprocal_rank = 1.0 / min(ranks) # Reciprocal rank of the earliest and correct retrieval
+        #min(ranks) is the rank of the first correctly retrieved answer
+        ranks_random_model = [ candidate_id_to_rank_random_model[candidate_id] for candidate_id in correct_candidates ]
         ##
-        mean_rr +=  reciprocal_rank 
+        mean_rr +=  (1.0/ min(ranks) ) 
+        mean_rr_random_model += (1.0/min(ranks_random_model) )
     ##
     no_of_questions = len( set(question_ids) )
     print("The Mean Average Precision is ",  mean_prec / valid_questions )
     print("The Mean Reciprocal Rank is "  ,  mean_rr / valid_questions )
+ 
+    print(".. for the Random Retriever ")
+    print("The Mean Average Precision is ",  mean_prec_random_model / valid_questions )
+    print("The Mean Reciprocal Rank is "  ,  mean_rr_random_model / valid_questions )
  
 
 ############################################
